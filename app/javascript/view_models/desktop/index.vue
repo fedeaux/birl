126b135af1045e9775455ff5988c3841bf2a1cf2
Pedro Bernardes <phec06@gmail.com>
2020-01-26 20:38:24 -0200

    desktop component close

--- app/javascript/view_models/desktop/index.vue
+++ app/javascript/view_models/desktop/index.vue
@@ -3,17 +3,24 @@
   #contents-wrapper
     #contents
       .desktop-component.default-container(v-for='view in views')
+        .desktop-component-close(@click='close(view)')
+          i.cancel.icon
+
         shared-view(:path='view')
 </template>
 
 <script lang="coffee">
   export default
     data: ->
-      views: ['/memes', '/tags', '/tags/14']
+      views: ['/memes', '/tags']
 
     methods:
       goTo: (data) ->
         @views.push data.to
+
+      close: (view) ->
+        index = @views.indexOf view
+        @views.splice index
 
     mounted: ->
       Global.events.$on 'Desktop::GoTo', @goTo
