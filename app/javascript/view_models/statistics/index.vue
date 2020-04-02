f40da29fed3a829ace5a5ee0caa88248fecd822f
Pedro Bernardes <phec06@gmail.com>
2020-04-02 02:07:49 -0300

    improv: Bringing Statistics to Life

--- app/javascript/view_models/statistics/index.vue
+++ app/javascript/view_models/statistics/index.vue
@@ -1,5 +1,9 @@
 <template lang="pug">
 .statistics.padded-2(v-if='days')
+  h1 Weeks
+  statistics-line(:day='day' :base=86400 v-for='day in days')
+  hr
+  h1 Days
   statistics-line(:day='day' :base=86400 v-for='day in days')
 </template>
 
